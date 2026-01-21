22d1cc1e55aad4315c01e37c03b803226bd3ff7b
OlexandrLukashenko <oleksandrlukashenko15@gmail.com>
2026-01-21 18:35:10 +0200

    Delete router.js

--- router.js
+++
@@ -1,8 +0,0 @@
-import {login} from 'login.js';
-
-export const router = VueRouter.createRouter({
-    history:VueRouter.createWebHashHistory(),
-    routes:[
-        {path: '/', name:'Sign in', component: login},
-    ]
-});
